f1a31703f01b93d8c7622ee8335b7ac358413775
dashmat <dashmathembram99@gmail.com>
2022-09-10 15:14:09 +0530

    name feature added

---
+++ face_recog.py
@@ -0,0 +1,52 @@
+import cv2
+import os
+
+if not os.path.exists('find_me'):
+    os.makedirs('find_me')
+
+id = 0
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+recognizer.read('./yml/train.yml')
+face_cascade_Path = "./yml/haarcascade_frontalface_default.xml"
+
+faceCascade = cv2.CascadeClassifier(face_cascade_Path)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+names = {3:'jimmy fermin',6: 'Michael Dam',51 : 'shouvik'} # add a name into this list
+
+trainPersonFileName = 'moj-krdi.jpg'
+
+
+while True:
+    img = cv2.imread('./find_me/%s' %trainPersonFileName)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
+        if (confidence < 100):
+            # print(id)
+            id = names[id]
+            confidence = "  {0}%".format(round(100 - confidence))
+        else:
+            # Unknown Face
+            id = "Not able to Match ?"
+            confidence = "  {0}%".format(round(100 - confidence))
+
+        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+
+    cv2.imshow('Output Image', img)
+    # Escape to exit the program
+    k = cv2.waitKey(10) & 0xff
+    if k == 27:
+        break
+print("\n [INFO] Exiting Program.")
+cv2.destroyAllWindows()
---
+++ take_face.py
@@ -0,0 +1,35 @@
+import cv2
+import os
+
+# Check if folder exists
+if not os.path.exists('images'):
+    os.makedirs('images')
+
+face_cascade = cv2.CascadeClassifier('./yml/haarcascade_frontalface_default.xml')
+count = 0
+
+face_id = input('\n enter user id (MUST be an integer) and press <return> -->  ')
+
+personFileName = 'Michael-Dam.jpg'
+
+while(True):
+    img = cv2.imread('./find_me/%s' %personFileName)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+        cv2.imwrite("./images/Users." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+        count += 1
+        # Save the captured image into the images directory
+        cv2.imshow('image', img)
+    # # Press Escape to end the program.
+    k = cv2.waitKey(100) & 0xff
+    if k < 30:
+        break
+    elif count >= 10:
+         break
+
+print("\n [INFO] Exiting Program.")
+
+
+
